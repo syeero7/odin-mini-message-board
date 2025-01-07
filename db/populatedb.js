872f97bd0d7feed7cf2746cb8cfe563f0fbcebe7
syeero7 <EmalHasitha@proton.me>
2025-01-07 14:41:55 +0530

    feat: update database population script

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,9 +1,8 @@
+import fs from "fs";
 import pg from "pg";
 const { Client } = pg;
 
 const databaseURL = process.env.DATABASE_URL;
-
-console.log(typeof databaseURL);
 
 const SQL = `CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -16,7 +15,14 @@ INSERT INTO messages (text, username)
 VALUES ('Hi there!', 'jane'), ('Hello World!', 'sam');
 `;
 
+const flagFilePath = "./scriptExecutedFlag.txt";
+
 const main = async () => {
+  if (fs.existsSync(flagFilePath)) {
+    console.log("The script has already executed once and will not run again.");
+    return;
+  }
+
   console.log("seeding...");
 
   const client = new Client({ connectionString: databaseURL });
@@ -24,6 +30,7 @@ const main = async () => {
   await client.query(SQL);
   await client.end();
 
+  fs.writeFileSync(flagFilePath, "Script has executed once.");
   console.log("done");
 };
 
